d2b10ac86608e312a1225605ab1229b7cfa4a8b8
Chris Olstrom <chris@olstrom.com>
2015-09-11 11:36:53 -0700

    Add spec/*

---
+++ spec/rakuna/validation/json_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe 'RakunaValidationJson' do
+  it 'fails' do
+    fail 'No tests written. Come on dude, you know better.'
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+require 'simplecov'
+
+module SimpleCov
+  # SimpleCov configuration
+  module Configuration
+    def clean_filters
+      @filters = []
+    end
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_profile 'test_frameworks'
+end
+
+ENV['COVERAGE'] && SimpleCov.start do
+  add_filter '/.rvm/'
+  add_filter '/.rubies/'
+  add_filter '/vendor/'
+end
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+require 'rakuna/validation/json'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
